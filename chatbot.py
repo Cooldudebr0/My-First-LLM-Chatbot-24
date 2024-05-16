65014f9181b4546d44bed091522140ddd629d27d
Cooldudebr0 <Evandmaddox@gmail.com>
2024-05-16 09:47:24 +0800

    hid the api key and allowed it to work automatically
    hid the api key

--- chatbot.py
+++ chatbot.py
@@ -5,11 +5,11 @@ import cohere
 # Adapted from the StreamLit OpenAI Chatbot example - https://github.com/streamlit/llm-examples/blob/main/Chatbot.py
 
 with st.sidebar:
-    # if hasattr(st, 'secrets'):
-    #     if 'COHERE_API_KEY' in st.secrets:
-    #         cohere_api_key = st.secrets['COHERE_API_KEY']
-    # else:
-    cohere_api_key = st.text_input("Cohere API Key", key="chatbot_api_key", type="password")
+    if st.secrets['API_KEY']:
+        cohere_api_key = st.secrets['API_KEY']
+    else:    
+        cohere_api_key = st.text_input("Cohere API Key", key="chatbot_api_key", type="password")
+
     st.markdown("[Get a Cohere API Key](https://dashboard.cohere.ai/api-keys)")
 
 st.title("💬 My First Chatbot")
@@ -25,7 +25,7 @@ if prompt := st.chat_input():
         st.info("Please add your Cohere API key to continue.")
         st.stop()
 
-    client = cohere.Client(api_key=cohere_api_key)
+    client = cohere.Client(api_key= cohere_api_key)
     st.chat_message("user").write(prompt)
     response = client.chat(model="command-r", 
                            chat_history=st.session_state.messages,
